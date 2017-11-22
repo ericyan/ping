0ab37da44e452db69058fd4dbd2139e84857dca6
Eric Yan <github@ericyan.me>
2017-11-22 22:08:06 +0800

    Stop the receiving goroutine before closing

--- pinger.go
+++ pinger.go
@@ -20,6 +20,7 @@ type Pinger struct {
 	id   int
 	conn *icmp.PacketConn
 	recv map[string]chan *message
+	stop chan bool
 }
 
 func NewPinger() (*Pinger, error) {
@@ -29,45 +30,54 @@ func NewPinger() (*Pinger, error) {
 		return nil, err
 	}
 	recv := make(map[string]chan *message)
-	p := &Pinger{id, conn, recv}
+	stop := make(chan bool)
+	p := &Pinger{id, conn, recv, stop}
 
 	go func(p *Pinger) {
 		buf := make([]byte, 1500)
 		for {
-			n, peer, err := p.conn.ReadFrom(buf)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
+			select {
+			default:
+				p.conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 
-			// Record receive time asap
-			now := time.Now()
-
-			msg, err := icmp.ParseMessage(ipv4.ICMPTypeEchoReply.Protocol(), buf[:n])
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-
-			if msg.Type == ipv4.ICMPTypeEchoReply {
-				reply := msg.Body.(*icmp.Echo)
-
-				// Ignore messages for other pingers
-				if reply.ID != p.id {
+				n, peer, err := p.conn.ReadFrom(buf)
+				if err != nil {
+					log.Println(err)
 					continue
 				}
 
-				// The first 32 bits of the ICMP message is not included in icmp.MessageBody
-				len := 4 + msg.Body.Len(ipv4.ICMPTypeEchoReply.Protocol())
-				log.Printf("%d bytes from %s: icmp_id=%d icmp_seq=%d\n", len, peer, reply.ID, reply.Seq)
+				// Record receive time asap
+				now := time.Now()
 
-				for dst, c := range p.recv {
-					if peer.String() == dst {
-						c <- &message{now, msg.Body, nil}
-					}
+				msg, err := icmp.ParseMessage(ipv4.ICMPTypeEchoReply.Protocol(), buf[:n])
+				if err != nil {
+					log.Println(err)
+					continue
 				}
-			} else {
-				log.Printf("got unknown ICMP message from %s: type=%d\n", peer, msg.Type)
+
+				if msg.Type == ipv4.ICMPTypeEchoReply {
+					reply := msg.Body.(*icmp.Echo)
+
+					// Ignore messages for other pingers
+					if reply.ID != p.id {
+						continue
+					}
+
+					// The first 32 bits of the ICMP message is not included in icmp.MessageBody
+					len := 4 + msg.Body.Len(ipv4.ICMPTypeEchoReply.Protocol())
+					log.Printf("%d bytes from %s: icmp_id=%d icmp_seq=%d\n", len, peer, reply.ID, reply.Seq)
+
+					for dst, c := range p.recv {
+						if peer.String() == dst {
+							c <- &message{now, msg.Body, nil}
+						}
+					}
+				} else {
+					log.Printf("got unknown ICMP message from %s: type=%d\n", peer, msg.Type)
+				}
+			case <-p.stop:
+				close(p.stop)
+				return
 			}
 		}
 	}(p)
@@ -119,5 +129,6 @@ func (p *Pinger) Ping(dst net.Addr) (float64, error) {
 }
 
 func (p *Pinger) Close() error {
+	p.stop <- true
 	return p.conn.Close()
 }

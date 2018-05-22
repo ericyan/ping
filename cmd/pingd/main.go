9134fc30cd16ce4f4b73e7e91690b3472297cdef
Eric Yan <github@ericyan.me>
2018-05-22 11:34:01 +0800

    Rename target to destination

--- cmd/pingd/main.go
+++ cmd/pingd/main.go
@@ -26,14 +26,14 @@ var (
 			Help:    "Ping round-trip time in seconds.",
 			Buckets: []float64{0.0005, 0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.2, 0.3, 0.5, 1},
 		},
-		[]string{"src", "target"},
+		[]string{"src", "dst"},
 	)
 	totalRequests = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "ping_requests_total",
 			Help: "Total number of ping requests sent.",
 		},
-		[]string{"src", "target"},
+		[]string{"src", "dst"},
 	)
 )
 
@@ -45,7 +45,7 @@ func init() {
 func main() {
 	flag.Parse()
 	if flag.NArg() < 1 {
-		log.Fatalln("please specify at least one target IP")
+		log.Fatalln("please specify at least one destination IP")
 	}
 
 	pinger, err := ping.NewPinger()
@@ -54,22 +54,22 @@ func main() {
 	}
 	defer pinger.Close()
 
-	for _, target := range flag.Args() {
-		ip := net.ParseIP(target)
+	for _, dst := range flag.Args() {
+		ip := net.ParseIP(dst)
 		if ip == nil {
-			log.Fatalln("invlid target IP:", target)
+			log.Fatalln("invlid destination IP:", dst)
 		}
 
 		go func(ip net.IP) {
 			for range time.Tick(time.Duration(*interval) * time.Second) {
 				rtt, err := pinger.Ping(&net.IPAddr{IP: ip})
 				if err != nil {
-					log.Printf("target=%s status=fail reason=%s", ip.String(), err)
+					log.Printf("dst=%s status=fail reason=%s", ip.String(), err)
 				} else {
-					rttHistogram.With(prometheus.Labels{"src": *bind, "target": ip.String()}).Observe(rtt / 1000)
+					rttHistogram.With(prometheus.Labels{"src": *bind, "dst": ip.String()}).Observe(rtt / 1000)
 				}
 
-				totalRequests.With(prometheus.Labels{"src": *bind, "target": ip.String()}).Inc()
+				totalRequests.With(prometheus.Labels{"src": *bind, "dst": ip.String()}).Inc()
 			}
 		}(ip)
 	}
